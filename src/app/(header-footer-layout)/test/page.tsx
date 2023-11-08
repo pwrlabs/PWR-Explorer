a001adb15301ca70d5558a835cb69ac335070735
loque18 <madridx2010@gmail.com>
2023-11-08 10:37:00 -0500

    feat: fixed sc not

--- src/app/(header-footer-layout)/test/page.tsx
+++ src/app/(header-footer-layout)/test/page.tsx
@@ -1,3 +1,19 @@
+import { BnToDec } from '@/shared/utils/formatters';
+import BigNumber from 'bignumber.js';
+
 export default function Page() {
-	return <main className="min-h-screen-2 box-border">Page</main>;
+	const v = 123;
+
+	function bn(value: string, decimals = 9, precision = 2) {
+		const bn = new BigNumber(value).shiftedBy(-decimals);
+		const float = bn.toFixed(precision);
+		const r = BigNumber(float).toFixed();
+		return r;
+	}
+
+	return (
+		<main className="min-h-screen-2 box-border">
+			<div className="container-2">{bn(v, 9, 9)}</div>
+		</main>
+	);
 }
