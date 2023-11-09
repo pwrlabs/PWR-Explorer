f63218ab106a1dc7b0a7257dd7adf255449635b4
loque18 <madridx2010@gmail.com>
2023-11-09 11:00:06 -0500

    feat: revert com

--- src/app/(header-footer-layout)/test/page.tsx
+++ src/app/(header-footer-layout)/test/page.tsx
@@ -1,12 +1,56 @@
-import React from 'react';
+function Table() {
+	return (
+		<table className="table-auto bg-awhite w-full ">
+			{/* table body */}
+			<tbody>
+				{new Array(10).fill(0).map((txn, idx) => (
+					<tr
+						key={idx}
+						className={` ${
+							idx % 2 == 0
+								? ' dark:bg-abrandc-dark-grey bg-abrandc-light-grey'
+								: 'bg-transparent'
+						}`}
+					>
+						{/* txn hash */}
+						<td className="xl:px-8 px-2 py-8">
+							<div className="flex gap-x-2 justify-start">
+								<div className="dark:text-ablue-300 text-ablue-200 font-medium">
+									0x000000000
+								</div>
+							</div>
+						</td>
 
-interface Header {
-	name: string;
-	thClass: string;
+						{/* block */}
+						<td className="xl:px-8 px-2 py-8">
+							<div className="dark:text-ablue-300 text-ablue-200 font-medium text-center block">
+								123
+							</div>
+						</td>
+
+						{/* time ago */}
+						<td className="xl:px-8 px-2 py-8">
+							<div className="dark:text-white text-abrandc-dark-grey font-normal text-center">
+								1 hour ago
+							</div>
+						</td>
+					</tr>
+				))}
+			</tbody>
+		</table>
+	);
 }
 
 export default function TestPage() {
-	return <div className="container-2 mx-auto"></div>;
+	return (
+		<div className="container-2 mx-auto">
+			<h1 className="text-black dark:text-white">Testosterona</h1>
+			<div className="flex gap-x-4">
+				<Table />
+				<Table />
+			</div>
+		</div>
+	);
 }
 
 // export default SkeletonTable;
