ad612daa9d441beb34cb81b9f1f8efc9942bad6b
ahmadhss366 <ahmadhss366@gmail.com>
2024-09-06 17:14:23 +0300

    fix chart

--- src/components/internal/chart/chart.component.tsx
+++ src/components/internal/chart/chart.component.tsx
@@ -4,143 +4,148 @@ import { ApexOptions } from 'apexcharts';
 const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
 type TransactionData = {
-	[key: number]: number;
+    [key: number]: number;
 };
 
 type TransactionChartProps = {
-	data: TransactionData;
+    data: TransactionData;
 };
 
 const TransactionChart = ({ data }: TransactionChartProps) => {
-	const [seriesData, setSeriesData] = useState<number[]>([]);
-	const [categories, setCategories] = useState<string[]>(['192']);
+    const [seriesData, setSeriesData] = useState<number[]>([]);
+    const [categories, setCategories] = useState<string[]>(['192']);
 
-	useEffect(() => {
-		if (data && Object.keys(data).length > 0) {
-			const dates = Object.keys(data).map((timestamp) => {
-				const date = new Date(parseInt(timestamp) * 1000);
-				return date.toLocaleDateString('en-US', {
-					month: 'short',
-					day: 'numeric',
-				});
-			});
-			const values = Object.values(data);
+    useEffect(() => {
+        if (data && Object.keys(data).length > 0) {
+            const dates = Object.keys(data).map((timestamp) => {
+                const date = new Date(parseInt(timestamp) * 1000);
+                return date.toLocaleDateString('en-US', {
+                    month: 'short',
+                    day: 'numeric',
+                });
+            });
+            const values = Object.values(data);
 
-			setCategories(dates);
-			setSeriesData(values);
-		}
-	}, [data]);
+            setCategories(dates);
+            setSeriesData(values);
+        }
+    }, [data]);
 
-	const minValue = Math.min(...seriesData);
-	const maxValue = Math.max(...seriesData);
+    const minValue = Math.min(...seriesData);
+    const maxValue = Math.max(...seriesData);
 
-	const options: ApexOptions = {
-		chart: {
-			type: 'area',
-			toolbar: { show: false },
-			zoom: { enabled: false },
-		},
-		dataLabels: { enabled: false },
-		stroke: { curve: 'smooth', width: 2, colors: ['#112FF8'] }, // Updated line color to #112FF8
-		grid: { show: false },
-		xaxis: {
-			type: 'category',
-			categories,
-			labels: {
-				show: true,
-				style: {
-					colors: '#737289', // Custom color for Y-axis labels (change to your desired color)
-					fontSize: '12px', // Adjust font size if necessary
-					fontWeight: 'bold',
-				},
-				rotate: 0, // Prevents diagonal rotation
-				formatter: function (value: any, arg2: any) {
-					// Show only the first day, middle day, and last day
-					const idx = categories.indexOf(value);
+    const options: ApexOptions = {
+        chart: {
+            type: 'area',
+            toolbar: { show: false },
+            zoom: { enabled: false },
+        },
+        dataLabels: { enabled: false },
+        stroke: { curve: 'smooth', width: 2, colors: ['#112FF8'] }, // Updated line color to #112FF8
+        grid: { show: false },
+        xaxis: {
+            type: 'category',
+            categories,
+            labels: {
+                show: true,
+                style: {
+                    colors: '#737289', // Custom color for Y-axis labels (change to your desired color)
+                    fontSize: '12px', // Adjust font size if necessary
+                    fontWeight: 'bold',
+                },
+                rotate: 0, // Prevents diagonal rotation
+                formatter: function (value: any, timestamp: any) {
+                    // Show only the first day, middle day, and last day
+                    const idx = categories.indexOf(value);
 
-					if (idx === 0 || idx === 6 || idx === 13) {
-						return value;
-					}
-					return '';
-				},
-			}, // Show x-axis labels
-			axisBorder: { show: false },
-			axisTicks: { show: false },
-		},
-		yaxis: {
-			show: true, // Show y-axis labels
-			labels: {
-				formatter: (value, ops) => {
-					if (value === minValue || value === maxValue) {
-						return `${value / 1000}k`;
-					}
-					return '';
-				},
-				style: {
-					colors: '#737289', // Custom color for Y-axis labels (change to your desired color)
-					fontSize: '12px', // Adjust font size if necessary
-					fontWeight: 'bold',
-				},
-			},
-			min: minValue,
-			max: maxValue,
-		},
-		tooltip: {
-			enabled: true,
-			x: {
-				formatter: (value, { dataPointIndex }) => {
-					return categories[dataPointIndex] ? categories[dataPointIndex] : 'N/A';
-				},
-			},
-		},
-		markers: {
-			size: 0, // No visible points on the chart
-		},
-		fill: {
-			type: 'gradient',
-			gradient: {
-				shade: 'light',
-				type: 'vertical',
-				shadeIntensity: 1,
-				gradientToColors: ['rgba(224, 223, 234, 0)'], // Transparent part of the gradient
-				inverseColors: false,
-				opacityFrom: 1, // Full opacity at the start
-				opacityTo: 0, // Fully transparent at the end
-				stops: [0, 100],
-				colorStops: [
-					{
-						offset: 0,
-						color: '#CCCCFE', // Start color (matching #CCCCFE)
-						opacity: 1,
-					},
-					{
-						offset: 100,
-						color: 'rgba(224, 223, 234, 0)', // End color
-						opacity: 0,
-					},
-				],
-			},
-		},
-		colors: ['#112FF8'], // Line color to #112FF8
-	};
+                    if (idx === 0 || idx === 6 || idx === 13) {
+                        return value;
+                    }
+                    return '';
+                },
+            }, // Show x-axis labels
+            axisBorder: { show: false },
+            axisTicks: { show: false },
+        },
+        yaxis: {
+            show: true, // Show y-axis labels
+            labels: {
+                formatter: (value) => {
+                    if (value === minValue || value === maxValue) {
+                        return `${value / 1000}k`;
+                    }
+                    return '';
+                },
+                style: {
+                    colors: '#737289', // Custom color for Y-axis labels (change to your desired color)
+                    fontSize: '12px', // Adjust font size if necessary
+                    fontWeight: 'bold',
+                },
+            },
+            min: minValue,
+            max: maxValue,
+        },
+        tooltip: {
+            enabled: true,
+            x: {
+                formatter: (value, { dataPointIndex }) => {
+                    return categories[dataPointIndex] ? categories[dataPointIndex] : 'N/A';
+                },
+            },
+            y: {
+                formatter: (value) => {
+                    return value.toString();
+                },
+            },
+        },
+        markers: {
+            size: 0, // No visible points on the chart
+        },
+        fill: {
+            type: 'gradient',
+            gradient: {
+                shade: 'light',
+                type: 'vertical',
+                shadeIntensity: 1,
+                gradientToColors: ['rgba(224, 223, 234, 0)'], // Transparent part of the gradient
+                inverseColors: false,
+                opacityFrom: 1, // Full opacity at the start
+                opacityTo: 0, // Fully transparent at the end
+                stops: [0, 100],
+                colorStops: [
+                    {
+                        offset: 0,
+                        color: '#CCCCFE', // Start color (matching #CCCCFE)
+                        opacity: 1,
+                    },
+                    {
+                        offset: 100,
+                        color: 'rgba(224, 223, 234, 0)', // End color
+                        opacity: 0,
+                    },
+                ],
+            },
+        },
+        colors: ['#112FF8'], // Line color to #112FF8
+    };
 
-	const series = [
-		{
-			name: 'Transactions',
-			data: seriesData.length ? seriesData : [0],
-		},
-	];
+    const series = [
+        {
+            name: 'Transactions',
+            data: seriesData.length ? seriesData : [0],
+        },
+    ];
 
-	return (
-		<div className="w-full h-48 bg-agrey-50 dark:bg-agrey-900 p-4 rounded-md">
-			<h1 className="text-agrey-600 text-sm font-medium leading-normal uppercase">
-				Transaction History in 14 Days
-			</h1>
-			<div className="">
-				<Chart options={options} series={series} type="area" width="100%" height={150} />{' '}
-			</div>
-		</div>
-	);
+    return (
+        <div className="w-full h-48 bg-agrey-50 dark:bg-agrey-900 p-4 rounded-md">
+            <h1 className="text-agrey-600 text-sm font-medium leading-normal uppercase">
+                Transaction History in 14 Days
+            </h1>
+            <div className="">
+                <Chart options={options} series={series} type="area" width="100%" height={150} />{' '}
+            </div>
+        </div>
+    );
 };
 
 export default TransactionChart;
