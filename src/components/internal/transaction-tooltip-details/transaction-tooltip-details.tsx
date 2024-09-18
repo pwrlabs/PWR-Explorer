b2af4b78ad917a59dc005d05826b85aa80f86ae8
ahmadhss366 <ahmadhss366@gmail.com>
2024-09-18 11:46:27 +0300

    fix issues

--- src/components/internal/transaction-tooltip-details/transaction-tooltip-details.tsx
+++ src/components/internal/transaction-tooltip-details/transaction-tooltip-details.tsx
@@ -5,13 +5,12 @@ import Image from 'next/image';
 import { BnToDec } from 'src/shared/utils/formatters';
 
 type TransactionTooltipDetailsProps = {
-
 	fee: string;
-	usdFee: number;
+	// usdFee: number;
 	nonce: string;
 };
 
-const TransactionTooltipDetails = ({ fee, usdFee, nonce }: TransactionTooltipDetailsProps) => {
+const TransactionTooltipDetails = ({ fee, nonce }: TransactionTooltipDetailsProps) => {
 	return (
 		<div className="flex flex-col gap-3 p-3 dark:bg-abrandc-dark-blackish bg-white text-agrey-900 dark:text-white rounded-2xl   w-60 z-index-3">
 			<div className="text-left p-1">
@@ -33,7 +32,8 @@ const TransactionTooltipDetails = ({ fee, usdFee, nonce }: TransactionTooltipDet
 			<div className="border-b border-agrey-800 p-1 text-left">
 				<p className="text-agrey-600">Transaction Fee:</p>
 				<p>
-					{BnToDec(fee, 9, 9)} <span>(${usdFee})</span>
+					{BnToDec(fee, 9, 9)}
+					{/* <span>(${usdFee})</span> */}
 				</p>
 			</div>
 			<div className="text-left p-1">
